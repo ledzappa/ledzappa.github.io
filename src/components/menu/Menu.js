75e340df60117fa41300409bbcef5a4f22c91d0f
ledzappa <juntti.mikael@gmail.com>
2020-10-21 17:17:19 +0200

    wip

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,42 +1,29 @@
+import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Menu.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandPointLeft } from '@fortawesome/free-solid-svg-icons'
 
-class Menu extends React.Component {
-  handleClick = () => {
-    this.setState({ showSidebar: !this.state.showSidebar });
-  };
+const Menu = (props) => {
+  const menuItems = [
+    { id: 'about', title: 'About' },
+    { id: 'projects', title: 'Projects' },
+    { id: 'techStack', title: 'TechStack' },
+  ];
 
-  render() {
-    const menuItems = [
-      { id: 'about', title: 'About', active: this.props.active === 'about' },
-      {
-        id: 'projects',
-        title: 'Projects',
-        active: this.props.active === 'projects',
-      },
-      {
-        id: 'techStack',
-        title: 'TechStack',
-        active: this.props.active === 'techStack',
-      },
-    ];
-    return (
-      <div className="menu">
-        <ul>
-          {menuItems.map((item, i) => (
-            <li id={item.id} onClick={this.props.onMenuClick} key={i}>
-              {item.title}{' '}
-              {item.active && (
-                <FontAwesomeIcon icon={faHandPointLeft} size="lg" />
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="menu">
+      <ul>
+        {menuItems.map((item, i) => (
+          <li id={item.id} onClick={props.onMenuClick} key={i}>
+            {item.title}{' '}
+            {props.active === item.id && (
+              <FontAwesomeIcon icon={faHandPointLeft} size="lg" />
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Menu;
