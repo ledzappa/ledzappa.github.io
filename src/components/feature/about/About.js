fecad9b691ae243165c13a3ee4cc5e5f81cd99ed
ledzappa <juntti.mikael@gmail.com>
2020-12-02 14:43:03 +0100

    refactored to functional components

--- src/components/feature/about/About.js
+++ src/components/feature/about/About.js
@@ -4,53 +4,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import me from '../../../assets/img/me.jpg';
 
-class About extends React.Component {
-  render() {
-    return (
-      <div className="page page-about">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-4 text-center">
-              <img
-                src={me}
-                className="rounded-circle img-fluid mb-4 mt-4 mt-sm-0"
-                alt="Me"
-              />
-            </div>
-            <div className="col-sm-8">
-              <h2>Hi!</h2>
-              <p>
-                My name is Mikael Juntti and I am a frontend developer with an
-                interest in design and UX. My current toolbox consists of
-                JavaScript/TypeScript, Angular, React.js, HTML/CSS, jQuery,
-                Bootstrap (among others). I'm always eager to learn more and to
-                develop my skills. Occasionally I also make guest appearances at
-                "the other side", perhaps writing some PHP and SQL. I strive to
-                write easy-to-read, slick and maintainable code, to challenge
-                myself and to surpass expectations.
-              </p>
-              <hr />
-              <ul className="list-unstyled">
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/mikael-juntti-a7666534/"
-                    target="_blank"
-                  >
-                    <FontAwesomeIcon className="mr-2" icon={faLinkedin} />
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.github.com/ledzappa" target="_blank">
-                    <FontAwesomeIcon className="mr-2" icon={faGithub} />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
+const About = () => (
+  <div className="page page-about">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-4 text-center">
+          <img
+            src={me}
+            className="rounded-circle img-fluid mb-4 mt-4 mt-sm-0"
+            alt="Me"
+          />
+        </div>
+        <div className="col-sm-8">
+          <h2>Hi!</h2>
+          <p>
+            My name is Mikael Juntti and I am a frontend developer with an
+            interest in design and UX. My current toolbox consists of
+            JavaScript/TypeScript, Angular, React.js, HTML/CSS, jQuery,
+            Bootstrap (among others). I'm always eager to learn more and to
+            develop my skills. Occasionally I also make guest appearances at
+            "the other side", perhaps writing some PHP and SQL. I strive to
+            write easy-to-read, slick and maintainable code, to challenge myself
+            and to surpass expectations.
+          </p>
+          <hr />
+          <ul className="list-unstyled">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/mikael-juntti-a7666534/"
+                target="_blank"
+              >
+                <FontAwesomeIcon className="mr-2" icon={faLinkedin} />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.github.com/ledzappa" target="_blank">
+                <FontAwesomeIcon className="mr-2" icon={faGithub} />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default About;
--- src/components/feature/tech-stack/TechStack.js
+++ src/components/feature/tech-stack/TechStack.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TechStack.css';
 
 const shuffle = (a) => {
@@ -12,98 +12,88 @@ const shuffle = (a) => {
   return a;
 };
 
-class TechStack extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { width: 0, height: 0 };
-  }
+const words = [
+  { text: 'JavaScript', value: 100 },
+  { text: 'TypeScript', value: 80 },
+  { text: 'Angular', value: 70 },
+  { text: 'RxJS', value: 30 },
+  { text: 'NGRX', value: 30 },
+  { text: 'Redux', value: 30 },
+  { text: 'Karma', value: 30 },
+  { text: 'Jest', value: 50 },
+  { text: 'Jasmine', value: 30 },
+  { text: 'Protractor', value: 30 },
+  { text: 'Angular.js', value: 60 },
+  { text: 'React.js', value: 60 },
+  { text: 'React Native', value: 35 },
+  { text: 'PHP', value: 50 },
+  { text: 'Java', value: 40 },
+  { text: 'Maven', value: 30 },
+  { text: 'Node.js', value: 40 },
+  { text: 'OpenShift', value: 30 },
+  { text: 'Jenkins', value: 30 },
+  { text: 'Docker', value: 30 },
+  { text: 'Github', value: 50 },
+  { text: 'Kibana', value: 50 },
+  { text: 'VsCode', value: 30 },
+  { text: 'IntelliJ', value: 30 },
+  { text: 'Photoshop', value: 50 },
+  { text: 'Linux', value: 40 },
+  { text: 'Bash', value: 40 },
+  { text: 'UX', value: 65 },
+  { text: 'CSS', value: 70 },
+  { text: 'HTML', value: 70 },
+  { text: 'SQL', value: 60 },
+  { text: 'Chrome Devtools', value: 60 },
+  { text: 'Bootstrap', value: 70 },
+  { text: 'Scrum', value: 35 },
+  { text: 'Kanban', value: 35 },
+  { text: 'Jira', value: 35 },
+];
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', () => this.updateWindowDimensions());
-  }
+const TechStack = () => {
+  const [size, setSize] = useState({ width: 0, height: 0 });
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', () => this.updateWindowDimensions());
-  }
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight });
+    };
 
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
+    updateWindowDimensions();
+    window.addEventListener('resize', () => updateWindowDimensions());
 
-  render() {
-    const words = [
-      { text: 'JavaScript', value: 100 },
-      { text: 'TypeScript', value: 80 },
-      { text: 'Angular', value: 70 },
-      { text: 'RxJS', value: 30 },
-      { text: 'NGRX', value: 30 },
-      { text: 'Redux', value: 30 },
-      { text: 'Karma', value: 30 },
-      { text: 'Jest', value: 50 },
-      { text: 'Jasmine', value: 30 },
-      { text: 'Protractor', value: 30 },
-      { text: 'Angular.js', value: 60 },
-      { text: 'React.js', value: 60 },
-      { text: 'React Native', value: 35 },
-      { text: 'PHP', value: 50 },
-      { text: 'Java', value: 40 },
-      { text: 'Maven', value: 30 },
-      { text: 'Node.js', value: 40 },
-      { text: 'OpenShift', value: 30 },
-      { text: 'Jenkins', value: 30 },
-      { text: 'Docker', value: 30 },
-      { text: 'Github', value: 50 },
-      { text: 'Kibana', value: 50 },
-      { text: 'VsCode', value: 30 },
-      { text: 'IntelliJ', value: 30 },
-      { text: 'Photoshop', value: 50 },
-      { text: 'Linux', value: 40 },
-      { text: 'Bash', value: 40 },
-      { text: 'UX', value: 65 },
-      { text: 'CSS', value: 70 },
-      { text: 'HTML', value: 70 },
-      { text: 'SQL', value: 60 },
-      { text: 'Chrome Devtools', value: 60 },
-      { text: 'Bootstrap', value: 70 },
-      { text: 'Scrum', value: 35 },
-      { text: 'Kanban', value: 35 },
-      { text: 'Jira', value: 35 },
-    ];
+    return () =>
+      window.removeEventListener('resize', () => updateWindowDimensions());
+  }, []);
 
-    return (
-      <div className="page page-tech-stack">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1 className="mt-4 mt-sm-0">&lt; TECH-STACK /&gt;</h1>
-              <hr />
-              <div className="text-center mb-4">
-                {shuffle(words).map((word, i) => (
-                  <div
-                    className="d-inline-block mr-3"
-                    style={{
-                      fontSize:
-                        word.value *
-                        (this.state.width > 1000
-                          ? 1
-                          : this.state.width > 600
-                          ? 0.6
-                          : 0.4),
-                      opacity: i % 2 === 1 ? 0.6 : 1,
-                    }}
-                    key={i}
-                  >
-                    {word.text}
-                  </div>
-                ))}
-              </div>
+  return (
+    <div className="page page-tech-stack">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="mt-4 mt-sm-0">&lt; TECH-STACK /&gt;</h1>
+            <hr />
+            <div className="text-center mb-4">
+              {shuffle(words).map((word, i) => (
+                <div
+                  className="d-inline-block mr-3"
+                  style={{
+                    fontSize:
+                      word.value *
+                      (size.width > 1000 ? 1 : size.width > 600 ? 0.6 : 0.4),
+                    opacity: i % 2 === 1 ? 0.6 : 1,
+                  }}
+                  key={i}
+                >
+                  {word.text}
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default TechStack;
